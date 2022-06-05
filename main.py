93b99ee60c1e9a30d449bd6c81d1852a6e3acbc1
TashaKo12 <belinda.kovalyova@yandex.ru>
2022-06-05 22:44:45 +0300

    download book covers

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import pathlib
 
 
 import requests
-
 from bs4 import BeautifulSoup
+from urllib.parse import unquote, urljoin, urlsplit
 from pathvalidate import sanitize_filename
 
 
@@ -13,7 +13,7 @@ def check_for_redirect(response):
         raise requests.exceptions.HTTPError
 
 
-def parse_book(number_book, url_book):
+def parse_book(number_book, url_book, template_url):
     response = requests.get(url_book.format(number_book))
     response.raise_for_status()
     check_for_redirect(response)
@@ -22,15 +22,22 @@ def parse_book(number_book, url_book):
     title_tag = soup.find(id="content").find('h1').text
     
     title_book, avtor_book = title_tag.split(" :: ")
-    avtor_book = avtor_book.strip()
-    title_book = title_book.strip()
-    print(title_book)
-    return title_book, avtor_book
-    #image=soup.find('img', class_='attachment-post-image')['src']
+
+    image_url = soup.find("div", class_="bookimage").find("img")["src"]
+    full_image_url = urljoin(template_url, image_url)
+
+    book_parametrs ={
+        "Название": title_book.strip(),
+        "Автор": avtor_book.strip(),
+        "Картинка": full_image_url,
+    }
+    
+    return book_parametrs
+
     #post_text = soup.find(class_="entry-content").text
 
 
-def save_book(response, filename, number_book, folder='С'):
+def save_book(response, filename, number_book, folder='books/'):
     pathlib.Path(folder).mkdir(parents=True, exist_ok=True)
     file_path =  os.path.join(folder, f"{number_book}.{sanitize_filename(filename)}.txt")
 
@@ -39,8 +46,23 @@ def save_book(response, filename, number_book, folder='С'):
         file.write(response.text)
 
 
+def download_image(image_url, folder="images/"):
+    pathlib.Path(folder).mkdir(parents=True, exist_ok=True)
+    response = requests.get(image_url)
+    response.raise_for_status()
+
+    filename = urlsplit(image_url).path.split("/")[-1]
+    filepath = os.path.join(folder, filename)
+
+    with open(unquote(filepath), "wb") as file:
+        file.write(response.content)
+    return f"Картинка: {filepath}"
+
+
+
 def main():
     
+    template_img_url = "http://tululu.org/images/nopic.gif"
     url_book = "https://tululu.org/b{}/"
     link_download_book = "https://tululu.org/txt.php"
 
@@ -50,14 +72,13 @@ def main():
         
         params = {"id": number_book}
         response = requests.get(link_download_book, params)
-        #response = requests.get(url_book.format(number_book))
         
         try:
             response.raise_for_status()
             check_for_redirect(response)
-            title_book, avtor_book = parse_book(number_book, url_book)
-            save_book(response, title_book, number_book)
-           
+            book_parametrs = parse_book(number_book, url_book, template_img_url)
+            save_book(response, book_parametrs["Название"], number_book)
+            download_image(book_parametrs["Картинка"])
             
         except requests.exceptions.HTTPError:
             print("Такой книги нет")

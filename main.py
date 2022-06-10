b5f3bc47780aba81b8001e841ef4ff8cc36c9ee5
TashaKo12 <belinda.kovalyova@yandex.ru>
2022-06-10 10:32:00 +0300

    Добавила устойчивость к разрыву сети

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import pathlib
 import argparse
-
+from time import sleep
 
 import requests
 from bs4 import BeautifulSoup
@@ -14,39 +14,38 @@ def check_for_redirect(response):
         raise requests.exceptions.HTTPError
 
 
-def parse_book_page(response, url_book, template_url):
-     
+def parse_book_page(response, template_url):
+
     soup = BeautifulSoup(response.text, 'lxml')
     title_tag = soup.find(id="content").find('h1').text
-    
-    title_book, avtor_book = title_tag.split(" :: ")
+
+    book_title, book_author = title_tag.split(" :: ")
 
     image_url = soup.find("div", class_="bookimage").find("img")["src"]
     full_image_url = urljoin(template_url, image_url)
 
-    comments_book = soup.find_all("div", class_="texts")
-    comments_texts_book = [comment_book.find("span", class_="black").text 
-                           for comment_book in comments_book]
+    book_comments = soup.find_all("div", class_="texts")
+    book_comments_texts = [comment_book.find("span", class_="black").text 
+                           for comment_book in book_comments]
 
     book_genres = soup.find("span", class_="d_book").find_all("a")
     book_genres = [genre_tag.text for genre_tag in book_genres]
 
 
     book_parametrs = {
-        "Название": title_book.strip(),
-        "Автор": avtor_book.strip(),
+        "Название": book_title.strip(),
+        "Автор": book_author.strip(),
         "Картинка": full_image_url,
-        "Комментарии": comments_texts_book,
+        "Комментарии": book_comments_texts,
         "Жанр": book_genres,
-
     }
-    
+
     return book_parametrs
 
 
-def save_book(response, filename, number_book, folder='books/'):
+def save_book(response, filename, book_number, folder='books/'):
     pathlib.Path(folder).mkdir(parents=True, exist_ok=True)
-    file_path =  os.path.join(folder, f"{number_book}.{sanitize_filename(filename)}.txt")
+    file_path =  os.path.join(folder, f"{book_number}.{sanitize_filename(filename)}.txt")
 
     with open(file_path, 'w', encoding="utf-8") as file:
         file.write(response.text)
@@ -65,7 +64,6 @@ def download_image(image_url, folder="images/"):
     return filepath
 
 
-
 def main():
     parser = argparse.ArgumentParser(
         description= "Проект скачивает книги и соответствующие им картинки,\
@@ -78,31 +76,32 @@ def main():
     args = parser.parse_args()
 
 
-    template_img_url = "https://tululu.org/shots/nopic.gif"
     url_book = "https://tululu.org/b{}/"
     link_download_book = "https://tululu.org/txt.php"
 
 
-    for number_book in range(args.start_id, args.end_id):
-        
-        params = {"id": number_book}
-        response = requests.get(link_download_book, params)
-        
+    for book_number in range(args.start_id, args.end_id):
+
+        params = {"id": book_number}
+        book_response = requests.get(link_download_book, params)
+
         try:
+            book_response.raise_for_status()
+            check_for_redirect(book_response)
+
+            response = requests.get(url_book.format(book_number))
             response.raise_for_status()
             check_for_redirect(response)
 
-            response = requests.get(url_book.format(number_book))
-            response.raise_for_status()
-            check_for_redirect(response)
-
-            book_parametrs = parse_book_page(response, url_book, template_img_url)
-            save_book(response, book_parametrs["Название"], number_book)
+            book_parametrs = parse_book_page(response, url_book)
+            save_book(book_response, book_parametrs["Название"], book_number)
             download_image(book_parametrs["Картинка"])
-            
+
         except requests.exceptions.HTTPError:
             print("Такой книги нет")
-            continue
+        except requests.exceptions.ConnectionError:
+            print("Повторное подключение к серверу")
+            sleep(20)
 
 
 if __name__ == "__main__":
